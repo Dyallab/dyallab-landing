440fd1aefd3025138690ad0a136f269327f4fdd8
Jonathan <contacto@jonathan.com.ar>
2025-10-04 22:26:08 -0300

    eventitos and trust us

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,5 +6,16 @@ import About from "./About.astro";
 import Contact from "./Contact.astro";
 import Footer from "./Footer.astro";
 import Products from "./Products.astro";
+import ClientsCarousel from "./ClientsCarousel.astro";
 
-export { Products, Layout, Header, Hero, Services, About, Contact, Footer };
+export {
+  Products,
+  Layout,
+  Header,
+  Hero,
+  Services,
+  About,
+  Contact,
+  Footer,
+  ClientsCarousel,
+};
